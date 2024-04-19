826211cae7f4bc796a0bfd80fd7f3c6b500fe94c
Kele <ebiritg@gmail.com>
2024-04-19 02:14:46 +0100

    Styled the Menu component but having a little issue, the images no longer renders after a adding the size prop

---
+++ crown-clothing/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Homepage from './pages/homepage/home-page.components';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Homepage />
+    </div>
+  );
+}
+
+export default App;
