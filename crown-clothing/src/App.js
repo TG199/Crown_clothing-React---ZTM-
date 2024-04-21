1a7349ac5b7d49a8407586b978dba4ef14a1c7fa
Kele <ebiritg@gmail.com>
2024-04-21 10:38:41 +0100

    Worked on Shop page and collection component

--- crown-clothing/src/App.js
+++ crown-clothing/src/App.js
@@ -1,13 +1,20 @@
-import React from 'react'
+import React from 'react';
+import {Routes, Route} from 'react-router-dom';
+
 import Homepage from './pages/homepage/home-page.components';
+import ShopPage from './pages/shop/shop.component';
 import './App.css';
+
 
 function App() {
   return (
     <div>
-      <Homepage />
-    </div>
-  );
-}
+      <Routes>
+        <Route exact path='/' element={<Homepage />} />
+        <Route path='/shop' element={<ShopPage />}/>
+      </Routes>
 
+    </div>
+);
+}
 export default App;
