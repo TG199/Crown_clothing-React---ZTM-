cad954711b01bb6ae9581b227e33d00cda058de8
Kele <ebiritg@gmail.com>
2024-05-08 17:15:50 +0100

    Updating the application with reselect library, replaced cart selectors with memorised selectors; git push

--- crown-clothing/src/components/cart-icon/cart-icon.component.jsx
+++ crown-clothing/src/components/cart-icon/cart-icon.component.jsx
@@ -5,19 +5,26 @@ import { connect } from 'react-redux'
 import { toggleCartHidden } from '../../redux/cart/cart.action'
 
 import { ReactComponent as ShoppingIcon } from '../../assests/shopping-bag.svg'
+import { selectCartItemsCount } from '../../redux/cart/cart.selectorss'
 
 import './cart-icon.styles.scss'
 
-const CartIcon = ({toggleCartHidden}) => (
+
+const CartIcon = ({ toggleCartHidden, itemCount}) => (
     <div className='cart-icon' onClick={toggleCartHidden}>
         <ShoppingIcon className='shopping-icon'>
-            <span className='item-count'>0
+            <span className='item-count'>{itemCount}
             </span>
         </ShoppingIcon>
     </div>
 )
+
 const mapDispatchToProps = dispatch => ({
     toggleCartHidden: () => dispatch(toggleCartHidden())
     
 })
-export default connect(null, mapDispatchToProps)(CartIcon)
+
+const mapStateToProps = (state) => ({
+    itemCount: selectCartItemsCount(state)
+})
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon)
